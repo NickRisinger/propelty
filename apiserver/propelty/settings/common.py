64f194f5e8da28a2589d50ed093d82807f189798
Nick Risinger <boozy044@gmail.com>
2024-09-02 00:47:33 +0300

    build(ci): update install dependencies for prod ci

--- apiserver/propelty/settings/common.py
+++ apiserver/propelty/settings/common.py
@@ -144,10 +144,10 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB"),
-            "USER": os.environ.get("POSTGRES_USER"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-            "HOST": os.environ.get("POSTGRES_HOST"),
+            "NAME": os.environ.get("POSTGRES_DB", "github-actions"),
+            "USER": os.environ.get("POSTGRES_USER", "github-actions"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "github-actions"),
+            "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
             "PORT": os.environ.get("POSTGRES_PORT", "5432"),
         }
     }
